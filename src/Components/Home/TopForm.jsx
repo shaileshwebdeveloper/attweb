743ae7ba49194f9b00c7c4a56325cdad97cc5927
shaileshwebdeveloper <shailesh.webdevelopment@gmail.com>
2023-05-16 17:08:43 +0530

    mad web app

---
+++ src/Components/Home/TopForm.jsx
@@ -0,0 +1,163 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    HStack,
+    InputRightElement,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Link,
+  } from '@chakra-ui/react';
+  import { useState } from 'react';
+  import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+  
+  export const TopForm = () => {
+    const [showPassword, setShowPassword] = useState(false);
+  
+    return (
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'} textAlign={'center'}>
+               Schedule a Demo
+            </Heading>
+            <Text fontSize={'lg'} color={'gray.600'}>
+            Switch to the Best Attendance Management App
+            </Text>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <HStack>
+                <Box>
+                  <FormControl id="firstName" isRequired>
+                    <FormLabel>First Name</FormLabel>
+                    <Input type="text" />
+                  </FormControl>
+                </Box>
+                <Box>
+                  <FormControl id="lastName">
+                    <FormLabel>Last Name</FormLabel>
+                    <Input type="text" />
+                  </FormControl>
+                </Box>
+              </HStack>
+
+              <HStack>
+                <Box>
+                  <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
+                  <Input type="email" />
+                  </FormControl>
+                </Box>
+                <Box>
+                <FormControl id="mobile" isRequired>
+                <FormLabel>Mobile</FormLabel>
+                <Input type="number" />
+                </FormControl>
+                </Box>
+              </HStack>
+
+
+              <HStack>
+                <Box>
+                  <FormControl id="employees" isRequired>
+                  <FormLabel>Employees</FormLabel>
+                  <select id="employees" name="employees">
+                    <option value="">No of employees</option>
+                    <option value="1">1-50</option>
+                    <option value="51">51-100</option>
+                    <option value="101">101-200</option>
+                    <option value="201">201-500</option>
+                    <option value="501">501-1000</option>
+                    <option value="1000">1000+</option>
+                  </select>
+                  </FormControl>
+                </Box>
+                <Box>
+                <FormControl id="mobile" isRequired>
+                <FormLabel>State</FormLabel>
+                {/* <!--- India states --> */}
+                <select id="country-state" name="country-state">
+                    <option value="">Select state</option>
+                    <option value="AN">Andaman and Nicobar Islands</option>
+                    <option value="AP">Andhra Pradesh</option>
+                    <option value="AR">Arunachal Pradesh</option>
+                    <option value="AS">Assam</option>
+                    <option value="BR">Bihar</option>
+                    <option value="CH">Chandigarh</option>
+                    <option value="CT">Chhattisgarh</option>
+                    <option value="DN">Dadra and Nagar Haveli</option>
+                    <option value="DD">Daman and Diu</option>
+                    <option value="DL">Delhi</option>
+                    <option value="GA">Goa</option>
+                    <option value="GJ">Gujarat</option>
+                    <option value="HR">Haryana</option>
+                    <option value="HP">Himachal Pradesh</option>
+                    <option value="JK">Jammu and Kashmir</option>
+                    <option value="JH">Jharkhand</option>
+                    <option value="KA">Karnataka</option>
+                    <option value="KL">Kerala</option>
+                    <option value="LA">Ladakh</option>
+                    <option value="LD">Lakshadweep</option>
+                    <option value="MP">Madhya Pradesh</option>
+                    <option value="MH">Maharashtra</option>
+                    <option value="MN">Manipur</option>
+                    <option value="ML">Meghalaya</option>
+                    <option value="MZ">Mizoram</option>
+                    <option value="NL">Nagaland</option>
+                    <option value="OR">Odisha</option>
+                    <option value="PY">Puducherry</option>
+                    <option value="PB">Punjab</option>
+                    <option value="RJ">Rajasthan</option>
+                    <option value="SK">Sikkim</option>
+                    <option value="TN">Tamil Nadu</option>
+                    <option value="TG">Telangana</option>
+                    <option value="TR">Tripura</option>
+                    <option value="UP">Uttar Pradesh</option>
+                    <option value="UT">Uttarakhand</option>
+                    <option value="WB">West Bengal</option>
+                </select>
+                </FormControl>
+                </Box>
+              </HStack>
+
+
+      
+
+              <Stack spacing={10} pt={2}>
+                <Button
+                  loadingText="Submitting"
+                  size="lg"
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                 DOWNLOAD OUR APP
+                </Button>
+              </Stack>
+              {/* <Stack pt={6}>
+                <Text align={'center'}>
+                  Already a user? <Link color={'blue.400'}>Login</Link>
+                </Text>
+              </Stack> */}
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TopSection } from '../Components/Home/TopSection'
+import { Footer } from '../Components/Home/Footer'
+
+export const Home = () => {
+
+
+  return (
+    <div>
+       <TopSection />
+       <Footer />
+    </div>
+  )
+}
